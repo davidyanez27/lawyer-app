86850d5f844991412d0734815b3f9f26f8d3d378
David Yanez <davidyanez-27@hotmail.com>
2023-09-18 16:24:30 -0600

    fet: clean request body

--- server/controllers/documents.ts
+++ server/controllers/documents.ts
@@ -1,54 +1,44 @@
-import { Request, Response, response } from "express";
-import PizZip from "pizzip";
-import Docxtemplater from "docxtemplater";
+import { Request, Response} from "express";
 import fs from "fs";
 import path from "path";
-
-
+import { readDoc } from "../libs/document";
 
 export const createDocument = async (req: Request, res: Response) => {
+  try {
+    const {name, and, name1} = req.body || '';
 
-    try {
-  // Load the docx file as binary content
-  const content = fs.readFileSync(
-    path.resolve(__dirname,'../templates',"autorizathion1.docx"),
-    "binary"
-  );
+    const doc = readDoc("autorizathion.docx");
 
-  const zip = new PizZip(content);
-
-  const doc = new Docxtemplater(zip, {
-    paragraphLoop: true,
-    linebreaks: true,
-  });
-
-  // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
-  doc.render(req.body);
-
-  const buf = doc.getZip().generate({
-    type: "nodebuffer",
-    // compression: DEFLATE adds a compression step.
-    // For a 50MB output document, expect 500ms additional CPU time
-    compression: "DEFLATE",
-  });
-
-  // buf is a nodejs Buffer, you can either write it to a
-  // file or res.send it with express for example.
-  fs.writeFileSync(path.resolve(__dirname,'../out', `gf.docx`), buf);
-
-res.json(req.body);
-    
-    } catch (error) {
-      if (typeof error === "object" && error && "message" in error && typeof error.message === "string") {
-        res.status(500).json({
-          message: error.message
-        });
+    const cleanData = Object.entries(req.body).reduce((acc, [key, value]) => {
+      if (value !== undefined) {
+        acc[key] = value;
       }
-      console.log(error);
-      
+      return acc;
+    }, {} as any);
+    
+    doc.render(cleanData);
+
+    const buf = doc.getZip().generate({
+      type: "nodebuffer",
+      compression: "DEFLATE",
+    });
+    fs.writeFileSync(path.resolve(__dirname, "../out", `autorizacion para salir del pais ${name}${and}${name1}.docx`), buf);
+
+    res.json(req.body);
+  } catch (error) {
+    if (
+      typeof error === "object" &&
+      error &&
+      "message" in error &&
+      typeof error.message === "string"
+    ) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
-  };
-  
+    console.log(error);
+  }
+};
 
 export const getDocument = async (req: Request, res: Response) => {};
 //export const createDocument = async (req: Request, res: Response) => {};
--- server/libs/document.ts
+++ server/libs/document.ts
@@ -15,11 +15,9 @@ interface User {
   document_number: string;
 }
 
-export function createAutorizathion (docxPath: string, request: Request) {
-  console.log(request);
+export function readDoc (docxPath: string) {
 
   // Load the docx file as binary content
-
   const content = fs.readFileSync(
     path.resolve(__dirname, "../templates", docxPath),
     "binary"
@@ -32,25 +30,6 @@ export function createAutorizathion (docxPath: string, request: Request) {
     linebreaks: true,
   });
 
-  const text = doc.getFullText(); // Gets the full text of the document
-  const matches = text.match(/{[^}]+}/g) || []; // Regex to match words inside {}
+  return doc;
 
-  // Remove the {} braces from each word
-  const keys = matches.map((match) => match.slice(1, -1));
-  //console.log(keys)
-
-  const requestBody = request.body;
-
-  doc.render({
-    name:'jesyus'
-  });
-
-  const buf = doc.getZip().generate({
-    type: "nodebuffer",
-    compression: "DEFLATE",
-  });
-
-  fs.writeFileSync(path.resolve(__dirname, "../out", `out.docx`), buf);
 }
-
-// {pronoun}, {name}, {age_pronoun}, {marital_status}, {nationality}, {address}{and}{name1}{age_pronoun}{marital_status}{nationality}{address}, por medio de la Presente {authorization}, a {son_pronoun} {son_name}{and}{son_name1}, con {document_type} {document_number}{and}{document_type1}{document_number1}, para que {can} salir del país y {travel_pronoun} a {destination}.- Razón por la cual {beg_pronoun} a las autoridades civiles, militares y migratorias de {country_pronoun} País, no poner ningún inconveniente a {son_pronoun} {son_name}{and}{son_name1}, al momento de salir del país y {travel_pronoun} a {destination}.
